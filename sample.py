f8ad04ea5b87ba744fa106185ee00de3b721a0fc
N R Shyamsundar Iyanger <nrshyamsundariyanger@protonmail.com>
2022-08-25 16:56:18 +0530

    finished the read and create operations on CRUD model

--- sample.py
+++ sample.py
@@ -1,25 +1,21 @@
-import enum
-import json
-d = {
-	"name" : "shyam",
-	"age" : "21",
-	"type" : "human"
-	}
-pet_list = []
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+engine=None
+DB_URL = "mysql+pymysql://estuate:estuate@localhost:3300/pets"
+engine = create_engine(DB_URL)
 
-try:
-    outputfile = open("database.json", "x")
-    print("Creating database.json file")
-    outputfile.close()
-except FileExistsError:
-    outputfile = open("database.json", "r")
-    print("Reading the database.json file")
-    try:
-        pet_dict = json.load(outputfile)
-        for i in range(0, len(pet_dict)):
-            print(pet_dict[str(i)])
-            pet_list.append(pet_dict[str(i)]["name"])
-            
-    except json.decoder.JSONDecodeError:
-        print("Error Decoding the json file")
-print(pet_list)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+metadata = MetaData()
+pets_table = Table(
+    "pets",
+    metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(30)),
+    Column('pet_type', String(30))
+)
+metadata.create_all(engine)
+print("The Column names for the created table are : ", pets_table.columns.keys())
+
--- server.py
+++ server.py
@@ -1,71 +1,96 @@
 from fastapi import FastAPI
+from sqlalchemy import MetaData, Table, Column, Integer, String, create_engine, sql
 from pydantic import BaseModel
 from typing import Union
 import json
+
 
 server = FastAPI()
 # Implementing simple CRUD operations for the Pet 
 # Pet will contain all the data required for the storage
 
 # BaseModel class from pydantic provides type validation, dictionary conversion
-# json conversion and many more helper functions for data transport and storage 
-class Pet(BaseModel):
+# json conversion and many more helper functions for data transport and storage
+class PetDetails(BaseModel):
     name:str
     age:int
-    type:Union[str,None] = None
+    pet_type:Union[str,None] = None
 
 
 # A temporary list to contain all the available pets 
-# the data stored in this list is written to database.json file in current directory
+# the data stored in this list is written to database.json file in current 
+# directory.
 # and the data is written to this file with each modification operation like 
 # create, update and delete.
 # the same file is read during the startup routine of the server.
 pets_list = []
 
-# function takes all the values present in "pets_list" list and then writes it to 
-# database.json file. 
-def write_pets():
-    outputfile =  open("database.json", "w")
-    pets = {}
-    for i in range(0, len(pets_list)):
-        pets[i] = dict(pets_list[i])
-    json.dump(pets, outputfile)
-    outputfile.close()
+# function takes all the values present in "pets_list" list and then writes it 
+# to database.json file. 
+#   def write_pets():
+#       outputfile =  open("database.json", "w")
+#       pets = {}
+#       for i in range(0, len(pets_list)):
+#           pets[i] = dict(pets_list[i])
+#       json.dump(pets, outputfile)
+#       outputfile.close()
 
 
-# function reads the database.json file in current directory and then converts the 
-# available data to a python dictionary.
-def read_pets():
-    try:
-        outputfile = open("database.json", "x")
-        outputfile.close()
-    except FileExistsError:
-        outputfile = open("database.json", "r")
-        try:
-            pet_dict = json.load(outputfile)
-            for i in range(0, len(pet_dict)):
-                pets_list.append(Pet(name=pet_dict[str(i)]["name"], age=pet_dict[str(i)]["age"], type=pet_dict[str(i)]["type"]))
-        except json.decoder.JSONDecodeError:
-            print("Error parsing the json file.")
-        return
+# function reads the database.json file in current directory and then converts 
+# the available data to a python dictionary.
+##  def read_pets():
+##      try:
+##          outputfile = open("database.json", "x")
+##          outputfile.close()
+##      except FileExistsError:
+##          outputfile = open("database.json", "r")
+##          try:
+##              pet_dict = json.load(outputfile)
+##              for i in range(0, len(pet_dict)):
+##                  pets_list.append(PetDetails(
+##                      name=pet_dict[str(i)]["name"], 
+##                      age=pet_dict[str(i)]["age"], 
+##                      type=pet_dict[str(i)]["type"])
+##                      )
+##          except json.decoder.JSONDecodeError:
+##              print("Error parsing the json file.")
+##          return
 
 # the startup function executes on after starting the server
 @server.on_event("startup")
 def startup():
-    read_pets()
+    DB_URL = "mysql+pymysql://estuate:estuate@localhost:3300/pets"
+    global engine
+    engine = create_engine(DB_URL)
+    metadata = MetaData()
+    global pets_table 
+    pets_table = Table(
+            "pets", 
+            metadata,
+            Column('id', Integer, primary_key=True, autoincrement=True),
+            Column('name', String(30)),
+            Column('age', Integer),
+            Column('type', String(30))
+            )
+    metadata.create_all(engine)
+    global connection 
+    connection = engine.connect()
+
 
 
 @server.on_event("shutdown")
 def shutdown_func():
+    connection.disconnect()
     write_pets()
 
 
 # index page for our website Lol :>
 @server.get("/")
 async def hello():
-	return {"text" : "hello"}
+	return "extravagant" 
 
-# 22-08-2022's assignment, implementing basic arithmetic operations and returning 
+# 22-08-2022's assignment, implementing basic arithmetic operations and 
+# returning 
 # the results to the user in json format
 @server.get("/arm_operations")
 async def arithmetic_operations(first_number:int, second_number:int):
@@ -76,22 +101,27 @@ async def arithmetic_operations(first_number:int, second_number:int):
         prod = first_number * second_number
         diff = first_number - second_number
         quo = first_number / second_number
-        return {"Sum" : sum, "Difference" : diff, "Product" : prod, "Quotient" : quo}
+        return {"Sum" : sum,
+                "Difference" : diff,
+                "Product" : prod,
+                "Quotient" : quo
+            }
 
 
 # the function executed on a post request to "/create" path
 # Parameters => all parameters passed are query parameters 
 #   name : refers to the name of the pet (required as there's no default value)
 #   age : refers to the age of the pet (required as there's no default value)
-#   type : refers to the breed of species of the pet (optional, "Unkown" acts as the default value)
+#   type : refers to the breed of species of the pet 
+#   (optional, "Unkown" acts as the default value)
 #
 # Return value => 
 #    A message saying the pet is added to the list 
 @server.post("/create")
-async def create_pet(pet:Pet):
-        pets_list.append(pet)
-        return {"msg" : f"{pet.name} is added to the list"}
-
+async def create_pet(pet:PetDetails):
+    stmt = pets_table.insert().values(name=pet.name, age=pet.age, type=pet.pet_type)
+    result = connection.execute(stmt)
+    return result
 
 # executes on a get request to "/read" path 
 # Parameters => no parameters required 
@@ -101,40 +131,28 @@ async def create_pet(pet:Pet):
 #   return a message if the "pets_list" list is empty 
 @server.get("/read")
 async def read_pets():
-    if len(pets_list) == 0:
-        return {"msg" : "No pets added yet"}
-    else:
-        pets = {}
-        for i in range(0, len(pets_list)):
-            pets[i] = dict(pets_list[i])
-        return pets
-
+    result = connection.execute(sql.select(pets_table))
+    pets = {}
+    for row in result:
+        pets[row.id] = {"name" : row.name, "age" : row.age, "type" : row.type}
+    return pets
 # executes on a get request to "/update" path 
 # Parameters => mixed parameters 
 #   index : refers to index of pet in "pets_list" list (path parameter)
 #   Query Parameters =>
-#       name : refers to the name of the pet (optional, "" acts as default value)
-#       age : refers to the age of the pet (optional, -1 acts as the default value)
-#       type : refers to the type of the pet (optional, "Unknown" acts as the default value)
+#    name : refers to the name of the pet (optional, "" acts as default value)
+#    age : refers to the age of the pet (optional,-1 acts as the default value)
+#    type : refers to the type of the pet 
+#    (optional, "Unknown" acts as the default value)
 #
 # Return value => 
 #   returns a message saying invalid index if the index is out of bounds
 #   returns a message saying pets details are updated
-@server.get("/update/{index}")
-async def update_pet(index:int, name:str="", age:int=-1, type:str="Unknown"):
-    try:
-        if name == "":
-            name = pets_list[index].name
-        if age == -1:
-            age = pets_list[index].age
-        if type == "Unknown":
-            type = pets_list[index].type
-        pets_list[index] = Pet(name=name, age=age, type=type)
-    except IndexError:
-        return {"msg" : "Invalid index"}
-    else:
-        return {"msg" : f"{name} is updated in the list"}
-
+@server.get("/update")
+async def update_pet(name:str, age:int, type:str):
+    stmt = pets_table.update().where(pets_table.c.name == name).values(age=age, type = type)
+    result = connection.execute(stmt)
+    return result
 # executes on a get request to "/delete" path 
 # Parameters => all parameters are query parameters 
 #   index : refers to the index of pet in the "pets_list" list.
@@ -143,17 +161,16 @@ async def update_pet(index:int, name:str="", age:int=-1, type:str="Unknown"):
 #   returns a message saying invalid index if the index is out of bounds 
 #   returns a message saying pet is deleted
 @server.get("/delete")
-async def delete_pet(index:int=0):
-    if index>len(pets_list)-1:
-        return {"msg" : "Invalid index"}
-    else:
-        del pets_list[index]
-        return {"msg" : f"Pet {index} deleted from the list"}
+async def delete_pet(name:str):
+    stmt = pets_table.delete().where(pets_table.c.name == name)
+    print(pets_table.name, " and ", name)
+    print(stmt)
+    result = connection.execute(stmt)
+    return result
 
 
 @server.get("/get")
-async def get_pet_details(index:int):
-    if index>len(pets_list)-1:
-        return {"msg" : "Invalid index"}
-    else:
-        return pets_list[index].dict()
+async def get_pet_details(name:str):
+    stmt = pets_table.select().where(pets_table.c.name == name)
+    result  = connection.execute(stmt)
+    return  result.fetchone()
