83c9bb2d501849a193581fac985400fea234d510
N R Shyamsundar Iyanger <nrshyamsundariyanger@protonmail.com>
2022-08-25 17:18:43 +0530

    seperating old code

--- server.py
+++ server.py
@@ -89,24 +89,6 @@ def shutdown_func():
 async def hello():
 	return "extravagant" 
 
-# 22-08-2022's assignment, implementing basic arithmetic operations and 
-# returning 
-# the results to the user in json format
-@server.get("/arm_operations")
-async def arithmetic_operations(first_number:int, second_number:int):
-    if second_number == 0:
-        return {"error" : "Division By Zero"}
-    else:
-        sum = first_number + second_number
-        prod = first_number * second_number
-        diff = first_number - second_number
-        quo = first_number / second_number
-        return {"Sum" : sum,
-                "Difference" : diff,
-                "Product" : prod,
-                "Quotient" : quo
-            }
-
 
 # the function executed on a post request to "/create" path
 # Parameters => all parameters passed are query parameters 
