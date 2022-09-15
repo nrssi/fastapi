82707dd8ca2e6d105fec4ff5636fcb0af536fcdb
N R Shyamsundar Iyanger <nrshyamsundariyanger@protonmail.com>
2022-09-15 16:31:51 +0530

    Adding a little detail to the index page

--- micro_app/main.py
+++ micro_app/main.py
@@ -9,4 +9,20 @@ app.include_router(retrieve.router)
 
 @app.get("/", response_class=HTMLResponse)
 def index():
-    return "<h1>Hello, World</h1>"
+    return """
+    <html>
+        <head>
+            <link href='https://fonts.googleapis.com/css?family=JetBrains Mono' rel='stylesheet'>
+            <style>
+            h1{
+                font-family: 'JetBrains Mono';
+                font-size: 22px;
+                text-align: center;
+            }
+            </style>
+        </head>
+        <body>
+            <h1>Hello, World</h1>
+        </body>
+    </html>"""
+
